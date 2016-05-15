13b846ea3033a33424ee8e3f3de04452810a9108
Blake Lockley <blakelockley@Blakes-MacBook-Pro.local>
2016-05-15 18:06:07 +1000

    Initial Commit

---
+++ TuringMachine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TuringMachine
+//
+//  Created by Blake Lockley on 15/05/2016.
+//  Copyright © 2016 Blake Lockley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
